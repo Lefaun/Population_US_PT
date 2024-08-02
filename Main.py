c7b8c7aaaf589ce0444bef24a4df2285685853e7
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-08-02 20:02:33 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -204,6 +204,13 @@ with col[0]:
         df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
         df_less_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference < -50000]
         
+        # KPIs
+        kpi1, kpi2, kpi3 = st.columns(3)
+        kpi1.metric(label="População Atual", value=int(population))
+        kpi2.metric(label="Nascimentos no último segundo", value=int(births))
+        kpi3.metric(label="Mortes no último segundo", value=int(deaths))
+
+        
         # % of States with population difference > 50000
         states_migration_greater= kpi2.metric(label="Nascimentos no último segundo", value=int(births))
         states_migration_less = kpi3.metric(label= "Mortes no último segundo", value=int(deaths))
